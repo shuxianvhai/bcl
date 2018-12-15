05f30645559d074ddd50ca6f1958c99d3db54638
demo1900k <403512248@qq.com>
2018-12-15 16:27:25 +0800

    tijiao

---
+++ pages/personalCenter/personalCenter.js
@@ -0,0 +1,19 @@
+//个人中心页
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    
+  },
+  onLoad: function () {
+    console.log('1231');
+  },
+  getUserInfo: function(e) {
+    
+  }
+})
